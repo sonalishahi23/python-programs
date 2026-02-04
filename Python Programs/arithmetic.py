654cb7569d4464297ccb9baecde65439f3bdbb5f
Sonali <sonalirajput160@gmail.com>
2026-02-04 20:16:11 +0530

    Delete Python Programs directory

--- Python Programs/arithmetic.py
+++
@@ -1,7 +0,0 @@
-value_a=int(input("Enter the Value of A: "))
-value_b=int(input("Enter the value of B: "))
-print("Sum Of A and B is ",value_a + value_b)
-print("Subtraction of A and B is ",value_a - value_b )
-print("Multiplication of A and B is ",value_a * value_b )
-print("Division of A and B is ",value_a / value_b )
-
